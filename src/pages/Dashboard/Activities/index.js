ebd4855142f3ec0309d6362b4ab61010c9676906
gabrielsd <gabrielsprecherdutra@gmail.com>
2023-05-12 00:12:35 -0300

    feat: improved activities layout

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -3,6 +3,9 @@ import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 import useToken from '../../../hooks/useToken';
 import DateButton from '../../../components/Dashboard/Activity/DateButton';
+import enter_icon from '../../../assets/images/enter_icon.png';
+import check_icon from '../../../assets/images/check_icon.png';
+import close_icon from '../../../assets/images/close_icon.png';
 
 // import useActivityDate from '../../../hooks/api/useActivityDate';
 // import useActivityByDate from '../../../hooks/api/useActivityDate';
@@ -34,7 +37,7 @@ export default function Activities() {
       <div Style='height: 100%;'>
         <a Style='font-size: 34px;'>Escolha de atividades</a>
         <ChooseDate error={dateError} dates={dates} chosenDate={chosenDate} setChosenDate={setChosenDate}/>
-        {(chosenDate === '') ? <div></div> : <ChooseActivity />}
+        {(chosenDate === '') ? <div></div> : <ChooseActivity date={chosenDate}/>}
       </div>
   );
 };
@@ -53,7 +56,7 @@ function ChooseDate(props) {
 
         <div Style='display: flex; margin-top: 23px;'>
           {props.dates.map((date, i) => (
-            <DateButton selected={props.chosenDate===date} onClick={props.setChosenDate(date)} key={i}> {date} </DateButton>
+            <DateButton selected={props.chosenDate===date} onClick={() => props.setChosenDate(date)} key={i}> {date} </DateButton>
           ))}
         </div>
       </div>
@@ -62,11 +65,24 @@ function ChooseDate(props) {
 }
 
 function ChooseActivity(props) {
+  const token = useToken();
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    activityApi.getByDate(token, props.date)
+      .then((response) => {
+        setActivities(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  }, props.date);
+
   return (
     <div Style='margin-top: 25px; display: flex; justify-content: space-around; padding: 35px;'>
-      <LocationContainer title='Auditório Principal'/>
-      <LocationContainer title='Auditório Lateral'/>
-      <LocationContainer title='Sala de Workshop'/>
+      <LocationContainer key={0} title='Auditório Principal' location='Principal' activities={activities}/>
+      <LocationContainer key={1} title='Auditório Lateral' location='Lateral' activities={activities}/>
+      <LocationContainer key={2} title='Sala de Workshop' location='Workshop' activities={activities}/>
     </div>
   );
 }
@@ -76,8 +92,18 @@ function LocationContainer(props) {
     <div Style='text-align: center;'>
       <a Style='font-size: 20px; color: #8E8E8E; font-weight: 400;'>{props.title}</a>
       <div Style='display:flex; justify-content: center;'>
-        <div Style='margin-top: 13px; border: 1px solid #D7D7D7; width: 18rem; min-height: 392px;'>
-          <ActivityCard title='soavekkk' init='2023-05-12T01:00:21.697Z' end='2023-05-12T02:30:21.697Z' vacancies='69'/>
+        <div Style='margin-top: 13px; border: 1px solid #D7D7D7; width: 18rem; min-height: 392px; padding: 14px;'>
+          {props.activities.map((activity, i) => (
+            activity.location === props.location ?
+              <ActivityCard
+                key={i}
+                title={activity.title}
+                init={activity.startsAt}
+                end={activity.endsAt}
+                id={activity.id}
+                vacancies={activity.capacity - activity._count.Subscription}
+              /> : <div></div>
+          ))}
         </div>
       </div>
     </div>
@@ -97,16 +123,23 @@ function ActivityCard(props) {
   const height = 80 * duration;
 
   return(
-    <div Style={`background: #F1F1F1; border-radius:5px; display:flex; justify-content: space-between; height: ${height}px;`}>
-      <div>
-        <div>{props.title}</div>
-        <div>{time}</div>
+    <div Style={`background: #F1F1F1; border-radius:5px; display:flex; justify-content: space-between; height: ${height}px; margin-bottom: 14px; padding: 12px;`}>
+      <div Style='font-size: 12px; text-align: left; color: #343434;'>
+        <div Style='font-weight: 700;'>{props.title}</div>
+        <div Style='margin-top: 6px;'>{time}</div>
       </div>
       <div Style='width: 66px; display: flex;'>
-        <div Style='height: 100%; width: 1px; display: flex;'>
-          <div Style='width: 1px; background: #CFCFCF; heigth: 80%;'/>
-        </div>
-        <div>{props.vacancies}</div>
+        <div Style='height: 100%; width: 2px; background: #CFCFCF;'/>
+        {props.vacancies === 0 ?
+          <div Style='width: 100%; height: 100%; display: flex; flex-direction: column; justify-content: center; align-items: center; padding-left: 12px;'>
+            <img src={close_icon} alt='enter' Style='cursor: pointer; height: 22px;'/>
+            <div Style='color: red; font-size: 9px;'>Esgotado</div>
+          </div> :
+          <div Style='width: 100%; height: 100%; display: flex; flex-direction: column; justify-content: center; align-items: center; padding-left: 12px;'>
+            <img src={enter_icon} alt='enter' Style='cursor: pointer; height: 22px;'/>
+            <div Style='color: green; font-size: 9px;'>{props.vacancies} vagas</div>
+          </div>
+        }
       </div>
     </div>
   );
